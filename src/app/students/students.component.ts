c7a58312c20a95284f8042708c465a2c6488655b
Lukasz <lukasz.piescicki@gmail.com>
2020-08-22 15:37:22 +0200

    Generate deployables

---
+++ src/app/students/students.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../student';
+import { StudentService } from '../student.service';
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+  students: Student[];
+  constructor(private studentService: StudentService) { }
+
+  ngOnInit(): void {
+    this.getStudents();
+  }
+  getStudents(): void {
+    this.studentService.getStudents()
+      .subscribe(students => this.students = students);
+  }
+  delete(student: Student): void {
+    this.students = this.students.filter(s => s !== student);
+    this.studentService.deleteStudent(student).subscribe();
+  }
+  add(name: string, email: string, username: string): void {
+    // Usunięcie białych znaków z danych
+    name = name.trim();
+    email = email.trim();
+    username = username.trim();
+  
+    // Zaprzestanie wykonywania, kiedy pola są puste
+    if (!name || !email) {
+      return;
+    }
+  
+    // Zaprzestanie wykonywania, kiedy adres e-mail nie zawiera "@"
+    if (email.indexOf('@') < 1) {
+      return;
+    }
+  
+    // Przesłanie danych do serwera i zaktualizowanie lokalnej tablicy
+      this.studentService.addStudent({ name, email, username } as Student)
+        .subscribe(student=> {
+          this.students.push(student);
+        });
+  }
+}
